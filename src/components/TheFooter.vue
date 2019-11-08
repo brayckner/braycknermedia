c6dac3d92234a2f379ffc63e5d7e19e1a3aa31a3
Carlos Rincon <mezerotm@gmail.com>
2019-11-08 14:55:17 -0500

    footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,74 +1,70 @@
 <template lang="pug">
-  div#contact_us.flex.flex-col.bg-brown-500.px-10
-    div.flex.justify-center.mt-4
-      p.text-sm.font-semibold.text-white Lets Connect
+    div#contact_us.flex.flex-col.bg-brown-500.px-10
+        div.flex.justify-center.mt-4
+            p.text-sm.font-semibold.text-white Lets Connect
 
-    form.flex.justify-center.mt-32.mb-24(name='contact', method='post', v-on:submit.prevent='handleSubmit', action='/success/', data-netlify='true', data-netlify-honeypot='bot-field')
-      div.flex.flex-col
+        form.flex.justify-center.mt-32.mb-24(name='contact', method='post', v-on:submit.prevent='handleSubmit', action='/success/', data-netlify='true', data-netlify-honeypot='bot-field')
+            div.flex.flex-col
 
-        input(type='hidden', name='form-name', value='contact')
-        p(hidden='')
-            label
-                | Don&rsquo;t fill this out:
-                input(name='bot-field')
+                input(type='hidden', name='form-name', value='contact')
+                p(hidden='')
+                    label
+                        | Don&rsquo;t fill this out:
+                        input(name='bot-field')
 
-        label.text-lg.font-semibold(for="services") Tell us what you are interested in:
-        select#services.rounded-lg.h-16.w-full.shadow-lg.mt-2.bg-white.p-5(required)
-          option(v-for="service in services" :value="service" :key="service" v-model="formData.service") {{ service }}
+                label.text-lg.font-semibold(for="services") Tell us what you are interested in:
+                select#services.rounded-lg.h-16.w-full.shadow-lg.mt-2.bg-white.p-5(required)
+                    option(v-for="service in services" :value="service" :key="service" v-model="formData.service") {{ service }}
 
-        div.my-4
-          label.text-xl.font-semibold(for="name") Name:
-          input#name.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="text" v-model="formData.name" required)
+                div.my-4
+                    label.text-xl.font-semibold(for="name") Name:
+                    input#name.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="text" v-model="formData.name" required)
 
-        div.my-4
-          label.text-xl.font-semibold(for="company_name") Company Name:
-          input#company_name.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="text" v-model="formData.company_name" required)
-          
-        div.my-4
-          label.text-xl.font-semibold(for="email") Email:
-          input#email.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="email" v-model="formData.email" required)
+                div.my-4
+                    label.text-xl.font-semibold(for="company_name") Company Name:
+                    input#company_name.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="text" v-model="formData.company_name" required)
 
-        div.my-4
-          label.text-xl.font-semibold(for="what_can_we_do") What can we do for you?
-          textarea#what_can_we_do.mb-16.rounded-lg.h-40.w-full.shadow-lg.mt-2.p-5(v-model="formData.message" required)
+                div.my-4
+                    label.text-xl.font-semibold(for="email") Email:
+                    input#email.rounded-lg.h-16.w-full.shadow-lg.mt-2.p-5(type="email" v-model="formData.email" required)
 
-        div.flex.justify-center
-          button.font-bold.text-xl.rounded-full.shadow-lg.w-48.h-12.bg-white Submit
+                div.my-4
+                    label.text-xl.font-semibold(for="what_can_we_do") What can we do for you?
+                    textarea#what_can_we_do.mb-16.rounded-lg.h-40.w-full.shadow-lg.mt-2.p-5(v-model="formData.message" required)
+
+                div.flex.justify-center
+                    button.font-bold.text-xl.rounded-full.shadow-lg.w-48.h-12.bg-white Submit
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      formData: {},
-      services: [
-        'User Experience',
-        'User Interface',
-        'Product Design',
-        'Development'
-      ]
-    }
-  },
-  methods: {
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-        )
-        .join("&");
+  export default {
+    data () {
+      return {
+        formData: {},
+        services: [
+          'User Experience',
+          'User Interface',
+          'Product Design',
+          'Development'
+        ]
+      }
     },
-    handleSubmit(e) {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: this.encode({
-          "form-name": e.target.getAttribute("name"),
-          ...this.formData
-        })
-      })
-        .then(() => this.$router.push("/success"))
-        .catch(error => alert(error));
+    methods: {
+      encode (data) {
+        return Object.keys(data).map(
+          key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        ).join('&')
+      },
+      handleSubmit (e) {
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: this.encode({
+            'form-name': e.target.getAttribute('name'),
+            ...this.formData
+          })
+        }).then(() => this.$router.push('/success')).catch(error => alert(error))
+      }
     }
   }
-};
 </script>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,7 +2,8 @@
   div.shadow-md.px-10.py-8.h-24.bg-white
     div.flex.justify-between
       div.flex.flex-col
-        g-image.w-64(src="../assets/images/logo.png" immediate="true")
+        g-link(to="/")
+            g-image.w-64(src="../assets/images/logo.png" immediate="true")
         div.flex.justify-center
           g-image.w-20.h-4(src="../assets/images/below_logo.png" immediate="true")
 
@@ -10,19 +11,18 @@
         svg.h-8.w-8(xmlns='http://www.w3.org/2000/svg', width='41', height='24.6', viewbox='0 0 41 24.6')
           path(d='M2,26v4.1H43V26ZM2,15.75v4.1H43v-4.1ZM2,5.5V9.6H43V5.5Z', transform='translate(-2 -5.5)')
 
-      transition(v-on:enter="enter")
-          div.fixed.top-0.right-0.z-20.pr-5.max-w-xs(v-show="isMenuOpen")
-            div.bg-white.rounded-lg.shadow-lg
-              div.flex.justify-end.pt-5.pr-5.mb-10(@click="isMenuOpen = !isMenuOpen")
-                svg(xmlns='http://www.w3.org/2000/svg', width='29.018', height='29.018', viewbox='0 0 29.018 29.018')
-                  path(d='M34.018,7.923,31.1,5,19.509,16.587,7.923,5,5,7.923,16.587,19.509,5,31.1l2.923,2.923L19.509,22.432,31.1,34.018,34.018,31.1,22.432,19.509Z', transform='translate(-5 -5)')
+      div.fixed.top-0.right-0.z-20.pr-5.max-w-xs(v-show="isMenuOpen")
+        div.bg-white.rounded-lg.shadow-lg
+          div.flex.justify-end.pt-5.pr-5.mb-10(@click="isMenuOpen = !isMenuOpen")
+            svg(xmlns='http://www.w3.org/2000/svg', width='29.018', height='29.018', viewbox='0 0 29.018 29.018')
+              path(d='M34.018,7.923,31.1,5,19.509,16.587,7.923,5,5,7.923,16.587,19.509,5,31.1l2.923,2.923L19.509,22.432,31.1,34.018,34.018,31.1,22.432,19.509Z', transform='translate(-5 -5)')
 
-              div.flex.flex-col.pl-5.pb-5.pr-16
-                a.font-medium.text-3xl.my-2.cursor-pointer(href="/#what_we_do" @click="isMenuOpen = !isMenuOpen") What We Do
-                a.font-medium.text-3xl.my-2.cursor-pointer(href="/#who_we_are" @click="isMenuOpen = !isMenuOpen") Who We Are
-                a.font-medium.text-3xl.my-2.cursor-pointer(href="/#product_design" @click="isMenuOpen = !isMenuOpen") Product Design
-                a.font-medium.text-3xl.my-2.cursor-pointer(href="/#development" @click="isMenuOpen = !isMenuOpen") Development
-                a.font-medium.text-3xl.my-2.cursor-pointer(href="/#contact_us" @click="isMenuOpen = !isMenuOpen") Contact Us
+          div.flex.flex-col.pl-5.pb-5.pr-16
+            a.font-medium.text-3xl.my-2.cursor-pointer(href="/#what_we_do" @click="isMenuOpen = !isMenuOpen") What We Do
+            a.font-medium.text-3xl.my-2.cursor-pointer(href="/#who_we_are" @click="isMenuOpen = !isMenuOpen") Who We Are
+            a.font-medium.text-3xl.my-2.cursor-pointer(href="/#product_design" @click="isMenuOpen = !isMenuOpen") Product Design
+            a.font-medium.text-3xl.my-2.cursor-pointer(href="/#development" @click="isMenuOpen = !isMenuOpen") Development
+            a.font-medium.text-3xl.my-2.cursor-pointer(href="/#contact_us" @click="isMenuOpen = !isMenuOpen") Contact Us
 </template>
 
 <script>
